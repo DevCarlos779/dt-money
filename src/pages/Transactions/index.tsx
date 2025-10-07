a0df1b03eba4a8def18a99b4367e62f8f4ed10a3
Carlos <devcarlos0112@gmail.com>
2025-10-07 07:09:59 -0300

    Criando HTML e CSS da Header, Summary e Table

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,31 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceText, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+    return (
+        <div>
+            <Header />
+            <Summary />
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de Software</td>
+                            <td><PriceText variant="income">R$ 1200,00</PriceText></td>
+                            <td>Venda</td>
+                            <td>12/10/2025</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td><PriceText variant="outcome">- R$ 50,00</PriceText></td>
+                            <td>Alimentação</td>
+                            <td>13/10/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        </div>
+    )
+}
